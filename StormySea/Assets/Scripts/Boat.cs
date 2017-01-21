d738dcb1d1ac2aba5ed485b5c03d6a97954bafba
Robert Wild <wildr2@rpi.edu>
2017-01-20 23:19:04 -0500

    Initial commit

---
+++ StormySea/Assets/Scripts/Boat.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boat : MonoBehaviour 
+{
+    private Ocean ocean;
+
+    private void Awake()
+    {
+        ocean = FindObjectOfType<Ocean>();
+    }
+
+    private void Update()
+    {
+        Vector2 pos = transform.position;
+        
+        float height0 = ocean.GetHeight(pos.x-0.1f);
+        float height1 = ocean.GetHeight(pos.x);
+        Vector2 tangent = new Vector2(pos.x, height1) - new Vector2(pos.x-0.1f, height0);
+        tangent = tangent.normalized;
+
+        pos.y = height1 + 0.5f;
+
+        transform.position = pos;
+        transform.rotation = Quaternion.Euler(0, 0, 
+            Mathf.Atan2(tangent.y, tangent.x) * Mathf.Rad2Deg);
+
+
+    }
+}
---
+++ StormySea/Assets/Scripts/Ocean.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ocean : MonoBehaviour
+{
+    // Waves
+    CompositeWave wave;
+
+    // Graphics
+    public LineRenderer line;
+    private int vertices_n = 1000;
+    private int line_length = 35;
+
+
+
+
+    public float GetHeight(float x)
+    {
+        float t = (x + line_length / 2f) / line_length;
+        return wave.GetHeight(t);
+    }
+
+
+    private void Awake()
+    {
+        wave = new CompositeWave();
+        wave.waves = new MovingWave[2];
+        wave.waves[0] = new MovingWave(1, 50, 2);
+        wave.waves[1] = new MovingWave(1, 30, -1);
+
+        line = GetComponent<LineRenderer>();
+        line.SetVertexCount(vertices_n);
+    }
+
+    private void UpdateWave()
+    {
+        for (int i = 0; i < vertices_n; ++i)
+        {
+            float t = (float)i / vertices_n;
+            float x = t * Mathf.PI * 2f;
+
+            float h = wave.GetHeight(t);
+
+            // Graphics
+            line.SetPosition(i, new Vector2(t * line_length - line_length / 2f, h));
+        }
+    }
+
+    private void Update()
+    {
+        UpdateWave();
+    }
+}
+
+public class MovingWave
+{
+    public float amplitude;
+    public float frequency;
+    public float speed;
+
+    public MovingWave(float a, float f, float s)
+    {
+        amplitude = a;
+        frequency = f;
+        speed = s;
+    }
+
+    /// <summary>
+    /// t from 0 to 1
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public float GetHeight(float t)
+    {
+        return Mathf.Sin(frequency * t + Time.time * speed) * amplitude;
+    }
+}
+
+public class CompositeWave
+{
+    public MovingWave[] waves;
+
+    public float GetHeight(float t)
+    {
+        float h = 0;
+        foreach (MovingWave wave in waves)
+        {
+            h += wave.GetHeight(t);
+        }
+        return h;
+    }
+}
